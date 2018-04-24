53b1b07cb1430e99e630c6e9894568d218b2eb3f
Stacey <staceynunez0@gmail.com>
2018-04-24 14:11:07 +0200

    Controller

---
+++ WebApplication1/Core/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Core.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Core.Controllers
+{
+    public class CategoryController
+    {
+        [HttpGet]
+        [Route("api/parentguardianreport")]
+        public async Task<Category[]> GetParentGuardianReports()
+        {
+            return (await ParentGuardianReportManager.GetParentGuardianReports())
+                .Select(x => new Category(x))
+                .ToArray();
+        }
+
+
+    }
+}
