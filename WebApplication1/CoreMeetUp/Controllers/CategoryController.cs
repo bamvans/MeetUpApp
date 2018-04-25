fb8d8a10d6f586f2ac494963ad1e873bf6d1ffdf
Stacey <staceynunez0@gmail.com>
2018-04-25 08:51:35 +0200

    Controller Creation.

--- WebApplication1/CoreMeetUp/Controllers/CategoryController.cs
+++ WebApplication1/CoreMeetUp/Controllers/CategoryController.cs
@@ -1,28 +1,79 @@
 ﻿using Core.Database.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 
 namespace CoreMeetUp.Controllers
 {
-        public class CategoryController : ApiController
+    public class CategoryController : ApiController
+    {
+        #region Get
+        public static async Task<Category[]> GetAllAsync()
         {
-            //private DatabaseContext _context;
-
-            [HttpGet]
-            [Route("api/category")]
-            public async Task<dynamic> GetCategories()
+            using (var db = new DatabaseContext())
             {
-                using (var db = new DatabaseContext())
-                {
-                    await db.SaveChangesAsync(); // if changes were made
-                    return db.Category.ToList();
-                }
+                return await db.Category.ToArrayAsync();
             }
-
-
         }
+
+
+        public Task<Category> GetById(int id)
+        {
+            using (var db = new DatabaseContext())
+            {
+                return db.Category.FirstOrDefaultAsync(x => x.Id == id);
+            }
+        }
+
+        #endregion
+
+        #region Create
+        public static async Task<int> Create(Category category)
+        {
+            using (var db = new DatabaseContext())
+            {
+                db.Category.Add(category);
+                await db.SaveChangesAsync();
+                return category.Id;
+            }
+        }
+
+        #endregion
+
+
+        #region Update
+
+        public static async Task Update(Category category)
+        {
+            using (var db = new DatabaseContext())
+            {
+                Category existing = await db.Category.FirstOrDefaultAsync(x => x.Id == category.Id);
+                if (existing == null) { throw new KeyNotFoundException(); }
+                existing.Name = category.Name;
+                await db.SaveChangesAsync();
+            }
+        }
+
+        #endregion
+
+        #region Delete
+
+        public static async Task Delete(Category category)
+        {
+            using (var db = new DatabaseContext())
+            {
+                Category existing = await db.Category.FirstOrDefaultAsync(x => x.Id == category.Id);
+                if (existing == null) { throw new KeyNotFoundException(); }
+                db.Category.Remove(existing);
+                await db.SaveChangesAsync();
+            }
+        }
+
+        #endregion
     }
+}
