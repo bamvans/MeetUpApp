d34027ad4da31cb713cde1b6e5f8dc381acae88b
Stacey <staceynunez0@gmail.com>
2018-05-03 10:35:14 +0200

    Added the necessary Interfaces and adjusted some components.

--- WebApplication1/WebApplication1/ClientApp/app/components/settings_screen/services/settingsscreen.service.ts
+++ WebApplication1/WebApplication1/ClientApp/app/components/settings_screen/services/settingsscreen.service.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { Router } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     templateUrl: './settingsscreen.component.html',
     styleUrls: ['./settingsscreen.component.css']
 })
 export class SettingsScreenService {
+
+    constructor(private http: Http, private router: Router, private sanitizer: DomSanitizer) {
+    }
+}
+
+export interface Category {
+    id: number;
+    name: string;
+    description: string;
+}
+
+export interface City {
+    id: number;
+    name: string;
+}
+
+export interface Group {
+    id: number;
+    name: string;
+    category?: Category;
+    city?: City;
 }
